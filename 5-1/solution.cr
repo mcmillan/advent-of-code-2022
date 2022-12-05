d704a6fa1ddbca80f2dc8bedae50473d5a7d00f0
Josh McMillan <josh@joshmcmillan.com>
2022-12-05 14:52:14 +0000

    Add day five solutions

---
+++ 5-1/solution.cr
@@ -0,0 +1,32 @@
+rows, instructions = File.read("input.txt")
+  .chomp
+  .split("\n\n")
+  .map { |s| s.split("\n") }
+
+stacks = rows.pop.strip.split(/\s+/).map { [] of Char }
+
+rows.each do |row|
+  row.chars.each_slice(4).map do |slice|
+    slice[1]
+  end.each_with_index do |item, index|
+    stacks[index] << item unless item.whitespace?
+  end
+end
+
+instructions.each do |instruction|
+  match = /move (\d+) from (\d+) to (\d+)/.match(instruction)
+
+  raise "can't parse `#{instruction}`" if match.nil?
+
+  count, from, to = match.captures.map { |num| num.to_s.to_i }
+
+  from_index = from - 1
+  to_index = to - 1
+
+  count.times do
+    item = stacks[from_index].shift
+    stacks[to_index].unshift(item)
+  end
+end
+
+puts stacks.map { |s| s.first }.join
